7756edfba5172f13c58698a4c08ff742da011081
y2jsheikh <y2jsheikh@hotmail.com>
2022-10-12 12:49:58 +0500

    Login Page Change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Roles;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,36 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Roles::truncate();
+
+        User::create([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'role_id' => 1,
+            'role' => 'Admin',
+            'email' => 'idola@mailinator.com',
+            'password' => '$2y$10$A6tpVXwbDfXDPGq62wG3BOof7xkPhOeiXlI11cjckCqSGFJznLfnq',
+            'remember_token' => 'NRgSnCUxKfc68WukdBWlDG7qv75uSP6cZhgn56HuTR4rCJzIBDHMhVhlDcu0',
+            'status' => 'Y',
+            'created_by' => 0,
+            'updated_by' => 0,
+            'created_at' => '2000-01-01 00:00:01',
+            'updated_at' => '2000-01-01 00:00:01'
+        ]);
+
+        Roles::create([
+            'name' => 'Admin',
+            'status' => 'N',
+            'created_at' => '2000-01-01 00:00:01',
+            'updated_at' => '2000-01-01 00:00:01'
+        ]);
+
+        Roles::create([
+            'name' => 'PD',
+            'status' => 'Y',
+            'created_at' => '2000-01-01 00:00:01',
+            'updated_at' => '2000-01-01 00:00:01'
+        ]);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,85 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Project Monitoring</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+</head>
+<body style="background-image: url({{asset('img/login-background.png')}});
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+    <section class="vh-100">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col col-xl-10">
+            <div class="card" style="border-radius: 1rem;">
+              <div class="row g-0">
+                <div class="col-md-6 col-lg-5 d-none d-md-block mt-5">
+                  <img src="{{asset('img/login-box-image.png')}}"
+                       alt="login form"
+                       class="img-fluid"
+                       style="border-radius: 1rem 0 0 1rem;" />
                 </div>
+                <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                  <div class="card-body p-4 p-lg-5 text-black">
+
+                    <form method="POST" action="{{ route('login') }}">
+                      @csrf
+
+                      <h3 class="fw-normal pb-2" style="letter-spacing: 1px; text-align: center;">User {{ __('Login') }}</h3>
+
+                      <div class="form-outline mb-4">
+                        <input type="text" name="username" id="form2Example17" class="form-control form-control-lg  @error('username') is-invalid @enderror" value="{{ old('username') }}" required autocomplete="username" autofocus />
+                        <label class="form-label" for="form2Example17">{{ __('Username') }}</label>
+
+                        @error('username')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                      </div>
+
+                      <div class="form-outline mb-4">
+                        <input type="password" name="password" id="form2Example27" class="form-control form-control-lg @error('password') is-invalid @enderror" required autocomplete="current-password" />
+                        <label class="form-label" for="form2Example27">{{ __('Password') }}</label>
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+
+                      </div>
+
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="exampleCheck1">{{ __('Remember Me') }}</label>
+                      </div>
+
+                      <div class="pt-1 mb-4 mt-3">
+                        <button class="btn btn-primary btn-lg btn-block" type="submit">{{ __('Sign In') }}</button>
+                      </div>
+
+                      @if (Route::has('password.request'))
+                        <a class="small text-muted" href="{{ route('password.request') }}">
+                          {{ __('Forgot Your Password?') }}
+                        </a>
+                      @endif
+                      <br>
+
+                    </form>
+
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-@endsection
+      </div>
+    </section>
+
+</body>
+</html>
