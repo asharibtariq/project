fea3499a6cecd4db29b9baf4e36568233cbfd569
mubashirkhaliq786 <60899776+mubashirkhaliq786@users.noreply.github.com>
2022-11-01 12:56:26 +0500

    Tabs Updated

--- resources/views/adminpanel/project/detail_tabs.blade.php
+++ resources/views/adminpanel/project/detail_tabs.blade.php
@@ -1,3 +1,4 @@
+{{--
 <ul class="nav nav-pills mb-3" role="tablist">
     <li class="nav-item">
         <a class="nav-link" href="#" role="tab" aria-selected="true">Project Director</a>
@@ -9,3 +10,56 @@
         <a class="nav-link" href="#" role="tab" aria-selected="false">Release</a>
     </li>
 </ul>
+--}}
+
+<div class="row">
+<ul class="nav nav-pills mb-3" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link " href="project_director.html" role="tab"
+           aria-controls="pills-Project-Director"
+           aria-selected="true">Project Director</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="allocation.html" role="tab"
+           aria-controls="pills-allocation"
+           aria-selected="false">Allocation</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="release.html" role="tab"
+           aria-controls="pills-release"
+           aria-selected="false">Release</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="comp_pc_1.html" role="tab"
+           aria-controls="pills-comp_pc_1"
+           aria-selected="false">Component as per PC-1</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="comp_nis.html" role="tab"
+           aria-controls="pills-comp_nis"
+           aria-selected="false">Component as per NIS</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="fy_wise_utilization.html" role="tab"
+           aria-controls="pills-fy_wise_utilization"
+           aria-selected="false">FY wise Utilization</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="pp_tests.html" role="tab"
+           aria-controls="pills-pp_tests" aria-selected="false">Project
+            Physical Tests</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="pc4_details.html" role="tab"
+           aria-controls="pills-pc4_details" aria-selected="false">PC-4
+            Details</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active " href="end_of_fy.html" role="tab"
+           aria-controls="pills-end_of_fy" aria-selected="false">End of
+            FY</a>
+    </li>
+</ul>
+</div>
+<hr/>
+<br/>
