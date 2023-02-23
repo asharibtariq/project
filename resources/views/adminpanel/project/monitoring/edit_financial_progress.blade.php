755572799d382637742a7c6e69e287f2530c9f4d
y2jsheikh <y2jsheikh@hotmail.com>
2023-02-23 11:54:21 +0500

    webcam js added (experimental)

--- resources/views/adminpanel/project/monitoring/edit_financial_progress.blade.php
+++ resources/views/adminpanel/project/monitoring/edit_financial_progress.blade.php
@@ -79,6 +79,11 @@
                                         <label for="multimedia">Instrument Annex <span class="text-danger">*</span></label>
                                         <input type="file" class="form-control" name="multimedia" id="images">
                                     </div>
+                                    <!--
+                                    <div id="my_camera"></div>
+                                    <input type=button value="Take Snapshot" onClick="take_snapshot()">
+                                    <div id="results" ></div>
+                                    -->
                                 </div>
                             </div>
 
@@ -99,7 +104,26 @@
         </div>
     </div>
 
+    {{--<script src="{{ asset('js/webcam.min.js') }}"></script>--}}
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.js"></script>
     <script>
+        Webcam.set({
+            width: 320,
+            height: 240,
+            image_format: 'jpeg',
+            jpeg_quality: 90
+        });
+        Webcam.attach( '#my_camera' );
+        <!-- Code to handle taking the snapshot and displaying it locally -->
+        function take_snapshot() {
+            // take snapshot and get image datao
+            Webcam.snap( function(data_uri) {
+                // display results in page
+                document.getElementById('results').innerHTML =
+                    '<img src="'+data_uri+'"/>';
+            } );
+        }
+
         $(document).ready(function () {
             $(document).on("change", "#component_id", function () {
                 var val = $(this).val();
